15d3db1b1717ed9b4dd55e912603317039cdcafa
Tony Tran <toant713@gmail.com>
2018-01-06 13:39:22 -0500

    Completed Day 15 part 2

--- src/main/java/day15/Day15.java
+++ src/main/java/day15/Day15.java
@@ -2,7 +2,6 @@ package day15;
 
 public class Day15 {
 
-
     public static long INPUT_GENERATORA = 783;
     public static long INPUT_GENERATORB = 325;
 
@@ -11,25 +10,29 @@ public class Day15 {
 
     public static final int FACTOR_A = 16807;
     public static final int FACTOR_B = 48271;
+    public static final int MULTIPLE_A = 4;
+    public static final int MULTIPLE_B = 8;
     public static final int DIVISOR = 2147483647;
 
-    public static int TOTAL_PAIRS = 40000000;
+
+    public static int BITS_TO_COMPARE = 16;
+    public static int PART1_TOTAL_PAIRS = 40000000;
+    public static int PART2_TOTAL_PAIRS = 5000000;
 
     public static void main(String[] args) {
-        System.out.println("PART1 answer for TEST: " + getJudgeCount(TEST_GENERATORA, TEST_GENERATORB));
-        System.out.println("PART1 answer for INPUT: " + getJudgeCount(INPUT_GENERATORA, INPUT_GENERATORB));
+        System.out.println("PART1 answer for TEST: " + getJudgeCount(TEST_GENERATORA, TEST_GENERATORB, PART1_TOTAL_PAIRS, 1, 1));
+        System.out.println("PART1 answer for INPUT: " + getJudgeCount(INPUT_GENERATORA, INPUT_GENERATORB, PART1_TOTAL_PAIRS, 1, 1));
+
+        System.out.println("PART2 answer for TEST: " + getJudgeCount(TEST_GENERATORA, TEST_GENERATORB, PART2_TOTAL_PAIRS, MULTIPLE_A, MULTIPLE_B));
+        System.out.println("PART2 answer for INPUT: " + getJudgeCount(INPUT_GENERATORA, INPUT_GENERATORB, PART2_TOTAL_PAIRS, MULTIPLE_A, MULTIPLE_B));
     }
 
-
-    public static int getJudgeCount(long inputA, long inputB) {
+    public static int getJudgeCount(long inputA, long inputB, int rounds, int multipleA, int multipleB) {
         int count = 0;
 
-        for (int i = 0; i < TOTAL_PAIRS; i++) {
-            long previousInputA = inputA;
-            long previousInputB = inputB;
-
-            inputA = (previousInputA * FACTOR_A) % DIVISOR;
-            inputB = (previousInputB * FACTOR_B) % DIVISOR;
+        for (int i = 0; i < rounds; i++) {
+            inputA = getJudgeNumber(inputA, multipleA, FACTOR_A);
+            inputB = getJudgeNumber(inputB, multipleB, FACTOR_B);
 
             if (isPair(inputA, inputB)) {
                 count++;
@@ -39,19 +42,28 @@ public class Day15 {
         return count;
     }
 
+    private static long getJudgeNumber(long input, int multiple, int factor) {
+        do {
+            long previousInput = input;
+            input = (previousInput * factor) % DIVISOR;
+        } while (input % multiple != 0);
+
+        return input;
+    }
+
     private static boolean isPair(long inputA, long inputB) {
         String binaryA = Long.toBinaryString(inputA);
         String binaryB = Long.toBinaryString(inputB);
 
-        if (binaryA.length() < 16 || binaryB.length() < 16) {
+        if (binaryA.length() < BITS_TO_COMPARE || binaryB.length() < BITS_TO_COMPARE) {
             return false;
         }
 
-        int shiftA = binaryA.length()-16;
-        int shiftB = binaryB.length()-16;
-        String binary16ofA =  binaryA.substring(shiftA);
-        String binary16ofB =  binaryB.substring(shiftB);
+        int shiftA = binaryA.length() - BITS_TO_COMPARE;
+        int shiftB = binaryB.length() - BITS_TO_COMPARE;
+        String binaryBITS_TO_COMPAREofA = binaryA.substring(shiftA);
+        String binaryBITS_TO_COMPAREofB = binaryB.substring(shiftB);
 
-        return binary16ofA.equals(binary16ofB);
+        return binaryBITS_TO_COMPAREofA.equals(binaryBITS_TO_COMPAREofB);
     }
 }
