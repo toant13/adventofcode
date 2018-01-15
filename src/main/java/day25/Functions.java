1425414f9d78d4894c9592b9a54ff2f085bab3e5
Tony Tran <toant713@gmail.com>
2018-01-15 00:30:32 -0500

    reformatted some things

--- src/main/java/day25/Functions.java
+++ src/main/java/day25/Functions.java
@@ -7,5 +7,4 @@ public interface Functions {
 
     Map<States, Consumer<Tape>> getFunctions();
 
-
 }
--- src/main/java/day25/States.java
+++ src/main/java/day25/States.java
@@ -1,16 +1,13 @@
 package day25;
 
 public enum States {
-    A("A"),
-    B("B"),
-    C("C"),
-    D("D"),
-    E("E"),
-    F("F");
+    A,
+    B,
+    C,
+    D,
+    E,
+    F;
 
-    String state;
-
-    States(String state) {
-        this.state = state;
+    States() {
     }
 }
